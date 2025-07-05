b44e24fadf55fb22089128f04658cb10110bc057
miko <1059665047@qq.com>
2025-07-05 15:54:34 +0800

    deploy

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: 'tech-stack-generator',
+  base: '/tech-stack-generator/',
   plugins: [vue()]
 })
